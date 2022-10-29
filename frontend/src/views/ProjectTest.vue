f744a9b5feed4c680c0b307a87f27de6dc0506f2
jiyeon12345 <wldys5353@naver.com>
2022-10-29 23:24:50 +0900

    Vue.js setting

---
+++ frontend/src/views/ProjectTest.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>테스트용</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "ProjectTest",
+  // components: {
+  //   ProjectTest,
+  // },
+};
+</script>
+<style lang=""></style>
