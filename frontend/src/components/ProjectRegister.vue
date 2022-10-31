5a39217f56451da91f9b2752510dbed3b0f87587
jiyeon12345 <wldys5353@naver.com>
2022-10-31 23:59:25 +0900

    [front]register 페이지 만들기

---
+++ frontend/src/components/ProjectRegister.vue
@@ -0,0 +1,42 @@
+<template lang="">
+  <div class="registerForm">
+    <h2>등록하기</h2>
+
+    <v-text-field
+      flat
+      v-model="hostName"
+      type="text"
+      dense
+      required
+      placeholder="이름 입력"
+    />
+    <v-text-field
+      flat
+      v-model="ip"
+      type="text"
+      dense
+      required
+      placeholder="ip 입력"
+    />
+  </div>
+</template>
+<script>
+export default {
+  name: "ProjectRegister",
+  data() {
+    return {
+      hostName: "",
+      ip: "",
+    };
+  },
+};
+</script>
+<style scoped>
+.registerForm {
+  display: grid;
+  place-items: center;
+}
+input {
+  width: 80%;
+}
+</style>
---
+++ frontend/src/views/ProjectRegisterPage.vue
@@ -0,0 +1,11 @@
+<template lang="">
+  <div><ProjectRegister /></div>
+</template>
+<script>
+import ProjectRegister from "@/components/ProjectRegister.vue";
+export default {
+  name: "ProjectRegisterPage",
+  components: { ProjectRegister },
+};
+</script>
+<style lang=""></style>
