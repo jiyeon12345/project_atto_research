0c7fef45163d710be70adf852c4346d27aa52644
jiyeon12345 <wldys5353@naver.com>
2022-11-01 18:48:12 +0900

    [fHostReg] 데이터 받아 return 과정 완료

--- frontend/src/components/ProjectRegister.vue
+++ frontend/src/components/ProjectRegister.vue
@@ -1,23 +1,34 @@
-<template lang="">
+<template>
   <div class="registerForm">
     <h2>등록하기</h2>
 
-    <v-text-field
-      flat
-      v-model="hostName"
-      type="text"
-      dense
-      required
-      placeholder="이름 입력"
-    />
-    <v-text-field
-      flat
-      v-model="ip"
-      type="text"
-      dense
-      required
-      placeholder="ip 입력"
-    />
+    <v-container>
+      <v-row style="padding: 20px 0 0 100px">
+        <v-text-field
+          flat
+          v-model="hostName"
+          type="text"
+          dense
+          required
+          placeholder="이름 입력"
+        />
+        <v-btn @click="isDuplicatedHostName">중복검사</v-btn>
+      </v-row>
+      <v-row style="padding: 20px 0 0 100px">
+        <v-text-field
+          flat
+          v-model="ip"
+          type="text"
+          dense
+          required
+          placeholder="ip 입력"
+        />
+        <v-btn @click="isDuplicatedIp">중복검사</v-btn>
+      </v-row>
+    </v-container>
+    <br />
+    <v-btn @click="submitData">등록하기</v-btn>
+    <v-btn @click="goToMainPage">뒤로가기</v-btn>
   </div>
 </template>
 <script>
@@ -29,14 +40,26 @@ export default {
       ip: "",
     };
   },
+  methods: {
+    isDuplicatedHostName() {
+      this.$emit("isDuplicatedHostName", this.hostName);
+    },
+    isDuplicatedIp() {
+      this.$emit("isDuplicatedIp", this.ip);
+    },
+    submitData() {
+      const { hostName, ip } = this;
+      this.$emit("submitData", { hostName, ip });
+    },
+    goToMainPage() {
+      this.$router.push({ name: "ProjectMain" });
+    },
+  },
 };
 </script>
 <style scoped>
 .registerForm {
   display: grid;
   place-items: center;
-}
-input {
-  width: 80%;
 }
 </style>
--- frontend/src/views/ProjectRegisterPage.vue
+++ frontend/src/views/ProjectRegisterPage.vue
@@ -1,11 +1,70 @@
-<template lang="">
-  <div><ProjectRegister /></div>
+<template>
+  <div>
+    <ProjectRegister
+      @isDuplicatedHostName="isDuplicatedHostName"
+      @isDuplicatedIp="isDuplicatedIp"
+      @submitData="submitData"
+    />
+  </div>
 </template>
 <script>
 import ProjectRegister from "@/components/ProjectRegister.vue";
+import axios from "axios";
+
 export default {
   name: "ProjectRegisterPage",
   components: { ProjectRegister },
+  methods: {
+    isDuplicatedHostName(payload) {
+      let hostName = payload;
+
+      axios
+        .get(`http://localhost:7777/project/checkHostName/${hostName}`)
+        .then((res) => {
+          if (res.data == true) {
+            alert("중복된 이름입니다.");
+          } else alert("사용 가능한 이름입니다.");
+        })
+        .catch(() => {
+          alert("조회 실패");
+        });
+    },
+    isDuplicatedIp(payload) {
+      let ip = payload;
+
+      axios
+        .get(`http://localhost:7777/project/checkIp/${ip}`)
+        .then((res) => {
+          if (res.data == true) {
+            alert("중복된 ip 입니다.");
+          } else alert("사용 가능한 ip입니다.");
+        })
+        .catch(() => {
+          alert("조회 실패");
+        });
+    },
+    submitData(payload) {
+      const { hostName, ip } = payload;
+      axios
+        .post(`http://localhost:7777/project/sign-up`, { hostName, ip })
+        .then((res) => {
+          if (res.data == 0) {
+            alert("등록을 완료했습니다.");
+            return this.$router.push({ name: "ProjectMain" });
+          }
+          if (res.data == 1) {
+            alert("등록된 수가 100개를 넘었습니다.");
+            return this.$router.push({ name: "ProjectMain" });
+          }
+          if (res.data == 2)
+            return alert("중복검사를 다시 해 주시기 바랍니다.");
+          if (res.data == 3) return alert("값을 모두 입력해 주시기 바랍니다.");
+        })
+        .catch(() => {
+          alert("등록에 실패하였습니다.");
+        });
+    },
+  },
 };
 </script>
 <style lang=""></style>
